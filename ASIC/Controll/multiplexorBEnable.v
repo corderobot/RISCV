66bf11584a88e871a7a0fd79058814054432ff41
corderobot <rodriscor@gmail.com>
2018-12-29 00:54:41 -0600

    Nueva ubicación y nueva extensión

--- ASIC/Controll/multiplexorBEnable.v
+++ ASIC/Controll/multiplexorBEnable.v
@@ -8,6 +8,6 @@ module multiplexorBEnable(opcode, enabledB);
   //n2 = bit 2 negado y asi sucesivamente
   not(n2, opcode[2]);
   
-//	Señal para indicar si se activa dicho multiplexor
+//	SeÃ±al para indicar si se activa dicho multiplexor
   nand(enabledB, n2, opcode[4], opcode[5]);
 endmodule
