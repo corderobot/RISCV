b108d4f6acec00541735998bea7a6ccae26c033d
KevinHern <35639443+KevinHern@users.noreply.github.com>
2019-01-14 19:28:47 -0600

    RISC V v1.0
    Creation of the module.
    Main processor.

---
+++ RTL/Modulos/riscv.v
@@ -0,0 +1,22 @@
+//---------------------------Galileo University---------------------------//
+//	Main Collaborators:
+//	- Rodrigo Cordero
+//	- Kevin Hernandez
+//
+//	Module Name: riscv.v
+//	Project: FPGA RISC V
+//	Description: Verilog module which describes the behavior of a RISC V processor
+//
+//
+//	Update History:
+//	- 01/14/2019: Creation of the module
+//
+//-------------------------------------------------------------------------//
+
+module riscv(clk, fetch_addr, instruction, result);
+	input clk;
+	output [31:0] fetch_addr, instruction, result;
+
+	
+
+endmodule
