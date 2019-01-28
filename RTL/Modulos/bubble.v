77363906b36b2e46ef14be52fc09fbade0911689
KevinHern <35639443+KevinHern@users.noreply.github.com>
2019-01-28 12:31:41 -0600

    Bubble v1.1
    Registers are now initialized with value 0

--- RTL/Modulos/bubble.v
+++ RTL/Modulos/bubble.v
@@ -10,6 +10,7 @@
 //
 //	Update History:
 //	- 01/14/2019: Creation of the module
+//	- 28/01/2019: Registers are now initialized with value 0.
 //
 //-------------------------------------------------------------------------//
 
@@ -17,6 +18,9 @@ module bubble(rs1, rw, loadEn, clk, clock);
   input [4:0] rs1, rw;
   input loadEn, clk;
   output reg clock;
+
+  initial clock = 0;
+
   always @(rs1, rw, loadEn, clk)
     if((rs1 == rw) & loadEn)
       clock = 0;
