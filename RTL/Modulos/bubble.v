a4b0461c554ca7f202cf536c474bf2b8955bf1b8
corderobot <rodriscor@gmail.com>
2019-02-25 18:05:25 -0600

    Inicialization with 0

--- RTL/Modulos/bubble.v
+++ RTL/Modulos/bubble.v
@@ -3,14 +3,14 @@
 //	- Rodrigo Cordero
 //	- Kevin Hernandez
 //
-//	Module Name: bubble.v
+//	Module Name: bubble.vhttps://www.edaplayground.com/x/42VM#design3
 //	Project: FPGA RISC V
 //	Description: Verilog module which describes the behavior for Risc V's bubble.
 //
 //
 //	Update History:
 //	- 01/14/2019: Creation of the module
-//	- 28/01/2019: Registers are now initialized with value 0.
+//	- 02/25/2019: Initialization with 0
 //
 //-------------------------------------------------------------------------//
 
@@ -18,9 +18,7 @@ module bubble(rs1, rw, loadEn, clk, clock);
   input [4:0] rs1, rw;
   input loadEn, clk;
   output reg clock;
-
   initial clock = 0;
-
   always @(rs1, rw, loadEn, clk)
     if((rs1 == rw) & loadEn)
       clock = 0;
